9fa19e4bd0dba558ee5094615948b9d1bb859eca
Ana Maria Granada <ana.granada1@udea.edu.co>
2025-09-13 15:32:51 -0500

    initial commit

---
+++ src/main/java/com/ep14/pet_manager/PetManagerApplication.java
@@ -0,0 +1,13 @@
+package com.ep14.pet_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ep14/pet_manager/PetManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ep14.pet_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

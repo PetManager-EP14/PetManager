5c5fb944a3fbebd2997a4ccf1c3d4d1b58384086
Ana Maria Granada <ana.granada1@udea.edu.co>
2025-09-21 21:42:01 -0500

    refactor: rename directory

--- src/main/java/com/ep14/pet_manager/repository/UserRepository.java
+++ src/main/java/com/ep14/pet_manager/repository/UserRepository.java
@@ -1,12 +1,15 @@
 package com.ep14.pet_manager.repository;
 
-import com.ep14.pet_manager.entity.Supplier;
+import java.util.Optional;
+import java.util.UUID;
+
 import com.ep14.pet_manager.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
-
 @Repository
-public interface UserRepository extends JpaRepository<User, UUID> {
+public interface UserRepository extends JpaRepository <User, UUID> {
+
+    Optional<User> findByEmail(String email);
+
 }
--- src/main/java/com/ep14/pet_manager/service/SecurityConfig.java
+++ src/main/java/com/ep14/pet_manager/service/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.ep14.pet_manager.Service;
+package com.ep14.pet_manager.service;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;

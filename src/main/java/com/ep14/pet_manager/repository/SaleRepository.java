12cb0797e00b68ada405f32be99fd74e691a0c2d
JuanJoM14 <juan.monsalveh1@udea.edu.co>
2025-10-17 20:59:17 -0500

    feat: add Sale repositories code

--- src/main/java/com/ep14/pet_manager/repository/SaleRepository.java
+++ src/main/java/com/ep14/pet_manager/repository/SaleRepository.java
@@ -2,8 +2,7 @@ package com.ep14.pet_manager.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import com.ep14.pet_manager.entity.SaleDetails;
+import com.ep14.pet_manager.entity.Sale;
 
 @Repository
-public interface SaleDetailsRepository extends JpaRepository<SaleDetails, Long> {}
+public interface SaleRepository extends JpaRepository<Sale, Long> {}
